3b42310aa11502f1e46d635bcd712b6adc6f1700
hoolaboy74 <hoolaboy@gmail.com>
2025-11-19 15:16:03 +0900

    changed: 개선된 플레이어 화면 - 오디오 더킹 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomeScreen from './screens/HomeScreen';
 import PlayerScreen from './screens/PlayerScreen';
+import PlayerScreenV2 from './screens/PlayerScreenV2';
 import BoardScreen from './screens/BoardScreen';
 import PostScreen from './screens/PostScreen';
 import CreatePost from './screens/CreatePost';
@@ -21,7 +22,8 @@ function App() {
             <Route path="board" element={<BoardScreen />} />
             <Route path="more" element={<MoreScreen />} />
           </Route>
-          <Route path="/video/:videoId" element={<PlayerScreen />} />
+          <Route path="/video/:videoId" element={<PlayerScreenV2 />} />
+          <Route path="/video-v1/:videoId" element={<PlayerScreen />} />
           <Route path="/board/create" element={<CreatePost />} />
           <Route path="/board/:postId" element={<PostScreen />} />
           <Route path="/admin" element={<Admin />} />
